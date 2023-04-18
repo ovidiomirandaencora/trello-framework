60f655663d38cfa1320b8b0a9bc6efa445cf2bef
Simon Ovidio Miranda Chiri <ovidiocbba@hotmail.com>
2023-04-18 18:46:23 -0400

    Update some code smells

--- src/main/java/com/ovidiomiranda/framework/pages/login/LoginPage.java
+++ src/main/java/com/ovidiomiranda/framework/pages/login/LoginPage.java
@@ -55,7 +55,7 @@ public class LoginPage extends BasePage {
    */
   public void setWebDriverUrl(final String url) {
     DriverManager.getInstance().getDriver().get(url);
-    LOGGER.info(String.format("Set URL: %s", url));
+    LOGGER.info("Set URL: {}", url);
   }
 
   /**
